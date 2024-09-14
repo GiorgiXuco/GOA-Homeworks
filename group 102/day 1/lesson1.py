38f1a95e4656a5a99ffd79256adca6072541ccc1
GiorgaXuco <giorgi241199@gmail.com>
2024-09-14 15:53:59 +0400

    first commit

---
+++ group 102/day 1/lesson1.py
@@ -0,0 +1,35 @@
+#name ="gio"
+#name არის ცვლადი
+# = არის ცვლადისტვის მნიშვნელობის მიმნიწებელი სიმბოლო
+#"gio" არის ცვლადისთვის მნიშვნელობა
+surname = "khutsishvili"
+
+#print(name)
+
+#print ფუნქციას გადაცემა ეკრანზე გამოსატანი ობიექტი 
+
+
+name = "gio"  #ეს არის str (string) ტიპის ცვლადი
+age = 14 #ეს არის float ტიპის ცვლადი ( ათწილადი)
+height = 175 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#blooleam (bool) ტიპის ცვლადი
+lives = "lives in Tbilisi"
+
+learning_programming = True  #True ან False
+Lives_in_Tbilisi = True
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+# print(name + " " + surname)
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + lives)
