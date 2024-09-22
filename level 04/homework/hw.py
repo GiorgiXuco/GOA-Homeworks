b909bf20c1188cd555d38384c0495f18c54637ed
GiorgiXuco <giorgi241199@gmail.com>
2024-09-22 19:11:21 +0400

    second commit

---
+++ level 04/homework/hw.py
@@ -0,0 +1,9 @@
+#დავალება: მომხმარებელს შემოატანინეთ 2 რიცხვი, ხოლო შემდეგ გაკეთეთ ამ რიცხვებზე ყველა მათემატიკური ოპერაცია ცალცალკე.
+
+num1 = int(input("enter number1: "))
+num2 = int(input("enter number2: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 // num2)
+print(num1 * num2)
