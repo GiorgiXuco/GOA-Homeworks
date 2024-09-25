7313221d3e5b1d624878dc4f5ae552983f94b7fe
GiorgiXuco <giorgi241199@gmail.com>
2024-09-25 23:35:12 +0400

    second commit

---
+++ level 05/homework/hw.py
@@ -0,0 +1,69 @@
+# #1)მომხმარებელს input ის საშუალებით შემოატანინეთ სახელი შემდეგ კი პროგრამა მას მიესალმოს 
+
+
+# name = str(input("enter your name: "))
+
+# print("hello" + " " + name)
+
+# #2)მომხმარებელს შემოატანინეთ სახელი შემდეგ გვარი და ტერმინალში დაბეჭდეთ მისი სრული სახელი და გვარი მოახდინეთ მათი კონკატინაცია
+
+# name = input("enter your name: ")
+# surname = input("enter your surname: ")
+
+# result = name + " " + surname
+
+# print(result)
+
+
+# #3)მომხმარებელს შეეკითხეთ სახელი, გვარი, ასაკი, საცხოვრებელი,ჰობი შემდეგ კი დაბეჭდეთ
+# #გრძელი წინადადება მაგ. შენი სახელია "სახელი და გვარი" შენი ასაკია "ასაკი" და ასე შემდეგ
+
+# name = input("enter your name: ")
+# surname = input("enter your surname: ")
+# age = input("enter your age: ")
+# city = input("enter your city: ")
+# hobby = input("enter your hobby: ")
+
+
+# result = "მე ვარ " + name + " " + surname + "მე ვარ " + age + "წლის " + "მე ვცხოვრობ " + city + "ში, " + "ჩემი ჰობია " + hobby
+
+# print(result)
+
+
+#4)მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი დაბეჭდეთ მომხმარებლის რიცხვს დამატებული 5
+
+
+# num1 = int(input("enter number: "))
+
+# result = num1 + 5
+
+# print(result)
+
+#5)მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი დაბეჭდეთ ამ რიცხვის კვადრატი
+
+# num2 = int(input("enter number: "))
+
+# print(num2 * num2)
+
+
+#6)მომხმარებელს შემოატანინეთ ორი რიცხვი შემდეგ კი ამ ორი რიცხვისგან შეადგინეთ
+#ოთხი მათემატიკური მოქმედება, დამატება გამოკლება გამრავლება და გაყოფა
+
+# num3 = int(input("enter number: "))
+# num4 = int(input("enter number2: "))
+
+
+# print(num3 + num4)
+# print(num3 - num4)
+# print(num3 * num4)
+# print(num3 // num4)
+
+
+
+#7)მომხმარებელს შეეკითხეთ მისი ასაკი შემდეგ კი დაბეჭდეთ 10 წლის შემდეგ რამდენი წლის იქნება
+
+
+# age = int(input("enter your age: "))
+
+# print("your age in 10 years: " + str(age + 10))
+
