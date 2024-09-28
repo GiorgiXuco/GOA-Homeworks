a5dbb92efc881de4227486b6096ea12e523efbf6
GiorgiXuco <giorgi241199@gmail.com>
2024-09-28 15:43:29 +0400

    second commit

---
+++ level 06/homework/hw8.py
@@ -0,0 +1,6 @@
+# 8) მომხმარებელს შემოატანინეთ პაროლი შემდეგ კითხეთ ხელახლა რომ გაიმეოროს პაროლი და შეამოწმეთ უდრის თუ არა ისინი ერთმანეთს
+
+num1 = input("enter your password: ")
+num2 = input("repeat your password: ")
+
+print(num1 == num2)
