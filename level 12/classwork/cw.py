b1144e2f5bbf8cd2e27dc3231497318a0c3685c1
GiorgiXuco <giorgi241199@gmail.com>
2024-10-13 10:32:04 +0400

    second commit

---
+++ level 12/classwork/cw.py
@@ -0,0 +1,24 @@
+#for loop
+#while loop
+
+
+#loop
+
+
+# for i in range(0, 20, 2):
+#     print(str(i) + " .Xuco")
+
+
+
+for i in range(1000):
+    print(str(i) + " .Xuco")
+
+
+# for i in range(200):
+#     if i % 2 == 0 :
+#         print(str(i) + " even ")
+#     else:
+#         print(str(i) + " odd ")
+
+
+
