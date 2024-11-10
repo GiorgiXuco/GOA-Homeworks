8db18d7fe2533f53a330f2a49058c93a757dcbf6
GiorgiXuco <giorgi241199@gmail.com>
2024-11-10 14:21:06 +0400

    second commit

--- level 22/homework/hw3.py
+++ level 22/homework/hw3.py
@@ -1,7 +1,22 @@
 #4) შექმენით 3 ცრილი სამივე იქნება განსხვავებული მონაცემთა ტიპიები  | ბოლეანი არა საჭირო | და შექმენით ცარიელი ცხრილი სადაც იქნება დასაწყისში ინტეჯერების ცხრილი  სტრინგების ცხრილი და მერე ფლოათების ცხრილი
 
-# num = [1,2,3,4,5]
-# num2 = [2.4,43.4,12.3,12.5,23.6]
-# num3 = ["giorgi", "khutsishvili"]
+num = [1,2,3,4,5]
+num2 = [2.4,43.4,12.3,12.5,23.6]
+num3 = ["giorgi", "khutsishvili"]
 
-# nums = [num1,num2,num3]
+nums = []
+
+for i in num:
+    if type(i) == int:
+        nums.append(i)
+print(nums)
+
+for i in num2:
+    if type(i) == float:
+        nums.append(i)
+print(nums)
+
+for i in num3:
+    if type(i) == str:
+        nums.append(i)
+print(nums)
---
+++ level 24/classwork/cw.py
@@ -0,0 +1,5 @@
+my_list = [1,2,3,4,5,6,7]
+
+my_list.append(8)
+my_list.remove(4)
+print(my_list)
