e0a578f1e70f8cf206f1c017019d1c1fd487bde6
GiorgiXuco <giorgi241199@gmail.com>
2025-02-10 19:14:23 +0400

    second commit

---
+++ level 64/classwork/sctipt.js
@@ -0,0 +1,65 @@
+
+
+
+// console.log("Giorgi Khutsishvili")
+// console.log("50")
+// console.log(50)
+// console.log(22.5)
+// console.log([1, 2, 3, 4, "g", "h", true, false])
+// console.log(true)
+// console.log(false)
+
+
+
+// console.log(typeof "hello")
+// console.log(typeof 50)
+// console.log(typeof 2.2)
+// console.log(typeof true)
+// console.log(num + 5)
+// console.log(2 *2)
+// console.log(5 - 3)
+// console.log(10 / 3)
+
+
+// var num = 5
+
+// var name = "Giorgi"
+
+// var surname = "gabrieli"
+
+// var myNumnersArr = [1, 2, 3, 4]
+
+
+
+
+// 1)
+// var userName = prompt("enter your name")
+// var userSurname = prompt("enter your surname")
+
+// console.log("hello" + " " + userName + " " + userSurname)
+
+
+// // 2)
+// var num1 = prompt("enter number1")
+// var num2 = prompt("enter number2")
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+
+// // 3)
+
+// var name = prompt("enter name")
+
+// alert("hello" + " " + name)
+
+
+
+
+
+
+console.log(5 > 3)
+console.log(5 >= 7)
+console.log(5 == "5")
+console.log(5 === "5")
