ae4a26d9dcf8ea6345185c79062e1d2c1ff7d280
GiorgiXuco <giorgi241199@gmail.com>
2025-03-20 19:08:37 +0400

    second commit

---
+++ level 78/homework/hw.js
@@ -0,0 +1,20 @@
+let currentSlide = 0;
+
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function updateSlide() {
+    const slider = document.querySelector('.slider');
+    slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateSlide();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateSlide();
+}
+
