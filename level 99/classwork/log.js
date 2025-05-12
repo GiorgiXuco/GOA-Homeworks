1ccaf1b955479250336d43f71ef00115d88470c1
GiorgiXuco <giorgi241199@gmail.com>
2025-05-12 20:57:49 +0400

    second commit

---
+++ level 99/classwork/log.js
@@ -0,0 +1,11 @@
+let accs = []
+
+const regForm = document.getElementById("regForm")
+
+regForm.addEventListener("submit", (e) => {
+    let name = e.target.name.value
+    let surname = e.target.surname.value
+    let email = e.target.email.value
+    let password = e.target.password.value
+    let repPassword = e.target.repPassword.value
+})
---
+++ level 99/classwork/script.js
@@ -0,0 +1,41 @@
+let time = document.querySelector(".time1")
+
+let same  = document.querySelector(".same")
+
+let clock = document.querySelector("#clock")
+
+let heartBut = document.querySelector("")
+
+let adress = ["/level 97/classwork/images/black.png","/level 97/classwork/images/red.png","/level 97/classwork/images/blue.png","/level 97/classwork/images/purple.png"]
+
+
+for(let i=0; i<same.lenght; i++){
+    same[i].addEventlisener("click", ()=>{
+        clock.src = adress[i]
+    })
+}
+
+
+
+
+
+setInterval(() => {
+    let date = new Date()
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+    let seconds = date.getSeconds()
+
+    seconds = seconds < 10 ? `0${seconds}` : seconds
+    
+    minute = minute < 10 ? `0${minute}` : minute
+    
+    hour = hour < 10 ? `0${hour}` : hour
+
+    time.textContent = `${hour}:${minute}:${seconds}`
+
+
+
+},1000)
+
+
+
